8f772639e0892d08edf4d681047dc8f15b6b8b18
agleevtimur <42980610+agleevtimur@users.noreply.github.com>
2019-11-01 12:58:56 +0300

    bot

---
+++ TelegramBot/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TlgTaxiBotApp.Controllers
+{
+    public class HomeController : Controller
+    {
+       public string Index()
+        {
+            return "Taxi 4 mesta";
+        }
+    }
+}
